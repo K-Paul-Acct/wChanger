d7ceb1bfdccd035adefb33f64b639f0e74baf508
Pavel <54417320+K-Paul-Acct@users.noreply.github.com>
2021-08-28 18:58:57 +0300

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -1,48 +1,37 @@
-import vk_api
-#from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
-#from vk_api.longpoll import VkLongPoll, VkEventType
-import datetime
-import time
+import vk_api, datetime, time, sched
 
-def IsParity(now):
-    sep = datetime.datetime(now.year if now.month >= 9 else now.year - 1, 9, 1)
-    d1 = sep - datetime.timedelta(days=sep.weekday())
+
+def NowIsOddWeek(shift_parity):
+    now = datetime.datetime.now() + datetime.timedelta(hours=3)
+    jan = datetime.datetime(now.year, 1, 1, 3)
+    d1 = jan - datetime.timedelta(days=jan.weekday())
     d2 = now - datetime.timedelta(days=now.weekday())
     return ((d2 - d1).days // 7 + shift_parity) % 2
 
-def ChangeName(new, time):
-    vk_session = vk_api.VkApi(token = TOKEN)
-    print(new)
-    print(time)
-    print()
-    vk_session.method('messages.editChat', {'chat_id': chat_id, 'title': new})
+def ChangeName(shift_parity, chat_id):
+    name = 'Группа 4232, '
+    now = datetime.datetime.now() + datetime.timedelta(hours=3)
+    
+    if (now.weekday() < 5) or (now.weekday() == 5 and now.hour < 18):
+        name += 'Нечёт.' if NowIsOddWeek(shift_parity) else 'Чёт.'
+        next_sat = now + datetime.timedelta(days=5-now.weekday())
+        next_changing_time = datetime.datetime(next_sat.year, next_sat.month, next_sat.day, 18)
+    else:
+        name += 'Чёт. След.' if NowIsOddWeek(shift_parity) else 'Нечёт. След.'
+        next_mon = now + datetime.timedelta(days=7-now.weekday())
+        next_changing_time = datetime.datetime(next_mon.year, next_mon.month, next_mon.day)
+    
+    vk_session.method('messages.editChat', {'chat_id': chat_id, 'title': name})
+        
+    s = sched.scheduler(time.time, time.sleep)
+    s.enterabs(time.mktime(next_changing_time.timetuple()), 1, ChangeName, argument=(shift_parity, chat_id,))
+    s.run()
+
 
 TOKEN = ''
-vk_session = vk_api.VkApi(token = TOKEN)
+vk_session = vk_api.VkApi(token=TOKEN)
 
-#my_groups = vk_session.method('messages.getConversations', {'group_id' : 202243542})
-# vk_session.method('messages.editChat', {'chat_id': 1, 'title': 'Group 1'})
-#print(my_groups)
-
-shift_parity = 1
-now = datetime.datetime.now() + datetime.timedelta(hours=3)
-last_change = datetime.datetime.weekday(now) == 0
-if datetime.datetime.weekday(now) == 6:
-    last_change = 1
+shift_parity = 0
 chat_id = 1
-#print(last_change)
 
-while True:
-    now = datetime.datetime.now() + datetime.timedelta(hours=3)
-    if last_change and datetime.datetime.weekday(now) == 0:
-        current_parity = 'Чёт' if IsParity(now) else 'Нечёт'
-        chat_name = 'Группа 4132, ' + current_parity
-        ChangeName(chat_name, time)
-        last_change = False
-    elif not last_change and datetime.datetime.weekday(now) == 5 and now.hour >= 22 and now.minute >= 30:
-        current_parity = 'Нечёт' if IsParity(now) else 'Чёт'
-        chat_name = 'Группа 4132, ' + current_parity + ' След'
-        #print(chat_name)
-        ChangeName(chat_name, time)
-        last_change = True
-    time.sleep(20)
+ChangeName(shift_parity, chat_id)
